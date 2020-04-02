f5033c2c9bd8da43de01031acdfc4781814d3878
Colin Brayton <gringolalia@gmail.com>
2020-04-02 15:51:32 -0300

    ionically themed

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+  module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/operation-consultation/'
+      : '/',
   lintOnSave: false,
   chainWebpack: config => {
     // config.plugins.delete('preload');
